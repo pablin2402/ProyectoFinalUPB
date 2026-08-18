b7f3a680a90da7cdc50d4a580de44a4436835bd4
Pablo Rivas <pabloperedo04@gmail.com>
2026-08-18 03:53:12 -0400

    Added new changes

--- PF-FRONTEND/my-project/src/view/ObjectiveRegionalsView.jsx
+++ PF-FRONTEND/my-project/src/view/ObjectiveRegionalsView.jsx
@@ -256,21 +256,23 @@ const ObjectiveRegionalsView = () => {
     }
   };
 
-  const deleteObjective = async (id2) => {
+const deleteObjective = async (id2) => {
     try {
-      const response = await axios.delete(
-        API_URL + "/whatsapp/order/objective/region/product",
-        { data: { _id: id2, id_owner: user } },
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
-      if (response.status === 200) {
-        fetchProducts(1);
-        setShowEditModal(false);
-      }
+        const response = await axios.delete(
+            API_URL + "/whatsapp/order/objective/region/product",
+            {
+                data: { _id: id2, id_owner: user },
+                headers: { Authorization: `Bearer ${token}` },
+            }
+        );
+        if (response.status === 200 || response.status === 204) {
+            fetchProducts(1);
+            setShowEditModal(false);
+        }
     } catch (error) {
-      console.error("Error al actualizar el estado de pago:", error);
+        console.error("Error al eliminar el objetivo:", error);
     }
-  };
+};
 
   const uploadProducts1 = async () => {
     try {
